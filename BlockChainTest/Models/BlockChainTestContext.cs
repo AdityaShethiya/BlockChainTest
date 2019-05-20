6c72503505811eac315f252e110074a632b461e6
Your Name <you@example.com>
2019-05-20 17:33:57 -0400

    first merge

---
+++ BlockChainTest/Models/BlockChainTestContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BlockChainTest.Models
+{
+    public partial class BlockChainTestContext : DbContext
+    {
+        public BlockChainTestContext()
+        {
+        }
+
+        public BlockChainTestContext(DbContextOptions<BlockChainTestContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-2PE26SM\\SQLADI;Database=aspnet-BlockChainTest-20190520024949;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+        }
+    }
+}
